ace98c8cef6b2194660326e3e26da5a7aeee04e0
daeseong <zang5989@gmail.com>
2024-05-21 15:11:50 +0900

    한 게시물 보기

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
+import { BoardView } from "./page/board/BoardView.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      { index: true, element: <BoardList /> },
+      { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
+    ],
+  },
+]);
 function App(props) {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-      children: [
-        { index: true, element: <BoardList /> },
-        { path: "write", element: <BoardWrite /> },
-      ],
-    },
-  ]);
   return (
     <ChakraProvider>
       <RouterProvider router={router} />
---
+++ src/page/board/BoardView.jsx
@@ -0,0 +1,59 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
+
+export function BoardView() {
+  // dynamic segment
+  const { id } = useParams();
+  const [board, setBoard] = useState(null);
+  useEffect(() => {
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
+  }, []);
+
+  /* DB의 값이 state 에 넘어오기 전에 랜더링 하여 null 오류 발생하는 경우 해결법*/
+  // board 가 null 일떄 return null or return <Spinner /> -> 로딩 느낌나게 함.
+  // board state 의 useState({})로 빈 객체 설정
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <Box>{board.id}번 게시물</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input value={board.title} readOnly={true}></Input>
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea value={board.content} readOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input value={board.writer} readOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성일시</FormLabel>
+            <Input type={"datetime-local"} value={board.inserted} readOnly />
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
