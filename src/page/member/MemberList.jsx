12ed8870a52fe1137adee17aea637dfa77b48124
daeseong <zang5989@gmail.com>
2024-05-22 15:09:50 +0900

    240522) 회원 목록 보기

--- src/page/member/MemberList.jsx
+++ src/page/member/MemberList.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Spinner, Table, Tbody, Td, Th, Tr } from "@chakra-ui/react";
-import * as PropTypes from "prop-types";
-
-function Thear(props) {
-  return null;
-}
-
-Thear.propTypes = { children: PropTypes.node };
+import axios from "axios";
 
 export function MemberList() {
   const [memberList, setMemberList] = useState([]);
+  useEffect(() => {
+    axios.get("/api/member/list").then((res) => {
+      setMemberList(res.data);
+    });
+  }, []);
+  /*빈 dependency 가 있어야 1번만 실행됨 */
   if (memberList.length === 0) {
     return <Spinner />;
   }
@@ -18,24 +18,22 @@ export function MemberList() {
       <Box>회원목록</Box>
       <Box>
         <Table>
-          <Thear>
-            <Tr>
-              <Th>#</Th>
-              <Th>별명</Th>
-              <Th>이메일</Th>
-              <Th>가입일시</Th>
-            </Tr>
-            <Tbody>
-              {memberList.map((member) => (
-                <Tr key={member.id}>
-                  <Td>{member.id}</Td>
-                  <Td>{member.email}</Td>
-                  <Td>{member.nickName}</Td>
-                  <Td>{member.inserted}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Thear>
+          <Tr>
+            <Th>#</Th>
+            <Th>별명</Th>
+            <Th>이메일</Th>
+            <Th>가입일시</Th>
+          </Tr>
+          <Tbody>
+            {memberList.map((member) => (
+              <Tr key={member.id}>
+                <Td>{member.id}</Td>
+                <Td>{member.nickName}</Td>
+                <Td>{member.email}</Td>
+                <Td>{member.signupDateAndTime}</Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
       </Box>
     </Box>
