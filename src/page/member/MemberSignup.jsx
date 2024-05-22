a81db6466ccee2e60a8257fab35b6124313393eb
daeseong <zang5989@gmail.com>
2024-05-22 10:25:01 +0900

    240522) 이메일 사용 가능한 지 확인 404 -> 불가 200 - > 가능

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -4,6 +4,8 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
@@ -22,11 +24,11 @@ export function MemberSignup() {
     setIsLoading(true);
     axios
       .post("/api/member/signup", { email, password, nickName })
-      .then((res) => {
+      .then(() => {
         toast({
           status: "success",
           description: "회원가입이 완료되었습니다.",
-          position: "top,",
+          position: "top",
         });
         navigate("/");
       })
@@ -50,6 +52,30 @@ export function MemberSignup() {
       });
   }
 
+  function handleCheckEmail() {
+    axios
+      .get(`/api/member/check?email="${email}`)
+      .then((res) => {
+        // 사용 할 수 있는 이메일
+        toast({
+          status: "info",
+          description: " 사용 가능한 이메일입니다",
+          position: "top",
+        });
+      })
+      // 사용 불가능 이메일
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: " 사용 불가능한 이메일입니다",
+            position: "top",
+          });
+        }
+      })
+      .finally();
+  }
+
   return (
     <Box>
       <Box>회원 가입</Box>
@@ -57,7 +83,14 @@ export function MemberSignup() {
         <Box>
           <FormControl>
             <FormLabel>이메일</FormLabel>
-            <Input onChange={(e) => setEmail(e.target.value)} />
+            <InputGroup>
+              <Input onChange={(e) => setEmail(e.target.value)} />
+              <InputRightElement w={"75px"} mr={1}>
+                <Button size={"sm"} onClick={handleCheckEmail}>
+                  중복 확인
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
         </Box>
         <Box>
