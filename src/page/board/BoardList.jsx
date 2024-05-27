1a9c495ee0ceeb31cc3cc8348bc0d0a994cb06c8
daeseong <zang5989@gmail.com>
2024-05-27 12:21:28 +0900

    20240527) 마지막 페이지네이션이 10개가 안되면 왼쪽을 채워넣음

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -56,6 +56,18 @@ export function BoardList() {
         </Table>
       </Box>
       <Box>
+        {pageInfo.prevPageNumber && (
+          <>
+            <Button onClick={() => navigate(`/?page=1`)}>처음</Button>
+            <Button
+              onClick={() => {
+                navigate(`/?page=${pageInfo.prevPageNumber}`);
+              }}
+            >
+              이전
+            </Button>
+          </>
+        )}
         {pageNumbers.map((pageNumber) => (
           <Button
             onClick={() => navigate(`/?page=${pageNumber}`)}
@@ -67,6 +79,20 @@ export function BoardList() {
             {pageNumber}
           </Button>
         ))}
+        {pageInfo.nextPageNumber && (
+          <>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.nextPageNumber}`)}
+            >
+              다음
+            </Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.lastPageNumber}`)}
+            >
+              맨끝
+            </Button>
+          </>
+        )}
       </Box>
     </Box>
   );
